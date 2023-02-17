c7635ea75cd359cbbe8b743f92e1f0d73d91f771
tumzy17 <stephamamize@gmail.com>
2023-02-17 11:46:35 -0800

    task 5

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+ * main - this is the main
+ *
+ * Return: 0 when successful
+ */
+
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,");
+	return (0);
+
+}
